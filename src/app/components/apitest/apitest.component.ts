0eddb4938e249b9839016885535ab8fe03e5e72f
Vishal Kotak <vishalkt07@gmail.com>
2017-12-08 14:15:55 -0500

    API Test Select Drop Off

--- src/app/components/apitest/apitest.component.ts
+++ src/app/components/apitest/apitest.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, NgZone, OnInit, ViewChild} from '@angular/core';
+import {FormControl} from '@angular/forms';
+import {MapsAPILoader} from '@agm/core';
 
 @Component({
   selector: 'app-apitest',
@@ -7,9 +9,63 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ApitestComponent implements OnInit {
 
-  constructor() { }
+  latitude: Number;
+  longitude: Number;
+  searchControl: FormControl;
+  zoom: number;
+  iconUrl: any;
+  dropoff: any;
+  @ViewChild('search')
+  searchElementRef: ElementRef;
+  constructor(private mapsAPILoader: MapsAPILoader,
+              private ngZone: NgZone) { }
 
   ngOnInit() {
+    this.latitude = 42.3404957;
+    this.longitude = -71.0878975;
+    this.iconUrl = 'http://maps.google.com/mapfiles/ms/icons/green-dot.png';
+    this.dropoff = '360 Huntington Avenue, Boston, MA, United States';
+    // create search FormControl
+    this.searchControl = new FormControl();
+
+    this.setCurrentPosition();
+
+    // load Places Autocomplete
+    this.mapsAPILoader.load().then(() => {
+      const autocomplete = new google.maps.places.Autocomplete(this.searchElementRef.nativeElement, {
+        types: ['address']
+      });
+      autocomplete.addListener('place_changed', () => {
+        this.ngZone.run(() => {
+          // get the place result
+          // console.log(autocomplete);
+          const place: google.maps.places.PlaceResult = autocomplete.getPlace();
+
+          // verify result
+          if (place.geometry === undefined || place.geometry === null) {
+            return;
+          }
+
+          // set latitude, longitude and zoom
+          this.latitude = place.geometry.location.lat();
+          this.longitude = place.geometry.location.lng();
+          this.dropoff = place.formatted_address;
+          this.zoom = 12;
+        });
+      });
+    });
+  }
+  updateDropoff() {
+    alert(this.dropoff);
+  }
+
+  private setCurrentPosition() {
+    if ('geolocation' in navigator) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        this.latitude = position.coords.latitude;
+        this.longitude = position.coords.longitude;
+      });
+    }
   }
 
 }
